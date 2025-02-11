306d25f5be6e889d1a5e5df4136fe868d2f3b947
Akili P V S S SRI SWAROOP KUMAR <sriswaroop12005@gmail.com>
2025-02-11 21:04:12 +0530

    Logo Updated

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className='flex items-center'>
+      <img className='w-[50px] h-[50px] mr-2' src='/images/logo.png' alt='Logo' />
       <h1 className='text-3xl font-bold transition-colors duration-300 ease-in-out hover:text-white'>
         <span className='text-white transition-colors duration-300 ease-in-out hover:text-black'>PER</span>
         <span className='text-black transition-colors duration-300 ease-in-out hover:text-white'> PLORA AI</span>
